5393e5dbfbcd82fa6b1fd7a31c16d90eaf141917
f-k-s <koschumacher@gmx.de>
2021-07-09 17:27:51 +0200

    test

--- ENABLE_captureNSP2stream_20210630.m
+++ ENABLE_captureNSP2stream_20210630.m
@@ -15,6 +15,7 @@ clear
 % trigger - testing
 % GUI bolus trigger - create
 
+
 %% initialize paths
 rootPth = fileparts(fileparts(mfilename('fullpath'))); % 'C:\Users\nradu\Documents\MATLAB';
 addpath(fullfile(rootPth,'DataStreamer','util'));
