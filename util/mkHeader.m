85edb19c0a9256b1ef6feb5808e599b7912f1f98
nrad-NIRS <konrad.schumacher@uniklinik-freiburg.de>
2022-03-11 12:28:33 +0100

    added Accelerometer support

--- util/mkHeader.m
+++ util/mkHeader.m
@@ -1,9 +1,13 @@
-function header = mkHeader(chnInfo,type)
+function header = mkHeader(chnInfo,type,nAcc)
 %
 
 if nargin<2 || isempty(type)
     type = 'rawHb';
 end
+if nargin<3 || isempty(nAcc)
+    nAcc = 0;
+end
+
 type = validatestring(type,{'rawHb','StO2'});
 
 header = 'TStamp,Trg,Frame';
@@ -24,9 +28,15 @@ switch type
         for prfx = {'StO2','HbO','HbR','muaWL1','muaWL2'}
             frmt = sprintf(',%s_S%%02dD%%02dD%%02dS%%02d',prfx{1});
             ptchHead = sprintf(frmt,chnInfo.');
-            cerebOxHead = sprintf('%s%s',cerebOxHead,ptchHead);
+            header = sprintf('%s%s',header,ptchHead);
         end
     otherwise, error('Bug');
+end
+
+for k = 1:nAcc
+    for var = {'acc%d_x','acc%d_y','acc%d_z','gyr%d_x','gyr%d_y','gyr%d_z'}
+        header = sprintf(['%s,' var{1}], header, k);
+    end
 end
 
 end
