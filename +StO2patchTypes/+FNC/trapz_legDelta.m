31c3c185a2b435d2e77f255f94870fa64afd2d65
nrad-NIRS <konrad.schumacher@uniklinik-freiburg.de>
2022-03-11 12:21:46 +0100

    trapz_legDelta: added parallel cable option (before othogonal was assumed)

--- +StO2patchTypes/+FNC/trapz_legDelta.m
+++ +StO2patchTypes/+FNC/trapz_legDelta.m
@@ -1,8 +1,23 @@
-function legDelta = trapz_legDelta(long, shrt, leg, delta)
-% calculate corrected leg length for isosceles trapezoid
+function legDelta = trapz_legDelta(long, shrt, leg, delta, orient)
+% calculates corrected leg length for isosceles trapezoid when cable
+% vertical to the 2 parallel sides. Set delta<0 for shorter leg and delta>0
+% for longer leg.
 
-trap_beta = @(long, shrt, leg, d) ...
-    pi/2 + d*asin( ((long-shrt)/2)/leg );
+if ~exist('orient','var')
+    orient = 'orthogonal';
+end
+
+orient = validatestring(orient,{'orthogonal','parallel'});
+
+switch orient
+    case 'orthogonal'
+        trap_beta = @(long, shrt, leg, d) ...
+            pi/2 + d*asin( ((long-shrt)/2)/leg );
+    case 'parallel'
+        trap_beta = @(long, shrt, leg, d) ...
+            (d>0)*pi - d*asin( ((long-shrt)/2)/leg );
+    otherwise, error('BUG');
+end
 
 legDelta = sqrt(delta^2 + leg^2 - ...
     2 * abs(delta) * leg * cos( ...
