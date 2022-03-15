aaedcc13b21a8ccf0c0ad4afb59381402f0befaa
nrad-NIRS <konrad.schumacher@uniklinik-freiburg.de>
2022-03-15 13:19:10 +0100

    VOT error checking

--- +StO2layouts/VOT220215.m
+++ +StO2layouts/VOT220215.m
@@ -5,7 +5,7 @@ defs = {
     [2 2 3 3], 'VOT220215.L_Rect36-18_asym';
     [1 1 3 3], 'VOT220215.L_Sqr36_asym';
     [4 5 6 6], 'VOT220215.R_Trap18-51-25_asym';
-    [5 5 6 7], 'VOT220215.R_Trap18-51-25_prec_sym';
+    [5 5 6 7], 'VOT220215.R_Trap18-51-25_prec_sym'; % _FALSE';
     [6 4 7 7], 'VOT220215.R_Sqr25_asym';
     [4 4 7 5], 'VOT220215.R_Sqr25_prec_sym';    
     };
