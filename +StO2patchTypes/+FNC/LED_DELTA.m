9558cc66d173ecdd5a8cff7dbaace0822603ca0a
nrad-NIRS <konrad.schumacher@uniklinik-freiburg.de>
2022-03-15 14:16:52 +0100

    Corrected distance between LED dies from .2 to .25, based on the mail from Lin today.

--- +StO2patchTypes/+FNC/LED_DELTA.m
+++ +StO2patchTypes/+FNC/LED_DELTA.m
@@ -1,6 +1,6 @@
 function delta = LED_DELTA
 % Returns half of the distance between the LED-dies for both LWLs
 
-delta = .2; % unit mm , half of the dies distance
+delta = .25; % unit mm , half of the dies distance
 
 end
