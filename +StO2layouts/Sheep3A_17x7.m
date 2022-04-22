b4ad117c328f66ce02d5a69d19d6d0420c31850b
f-k-s <koschumacher@gmx.de>
2022-04-22 15:10:40 +0200

    added sheep3a patch definitions

---
+++ +StO2layouts/Sheep3A_17x7.m
@@ -0,0 +1,109 @@
+function [Optodes, Types] = Sheep3A_17x7
+% 3rd Sheep
+
+% TRAPEZOIDS (HEAD)
+patches = {
+    % horizontal:
+    1 2 9 9     'head_trap_oo'; % othogonal, outward
+    2 2 3 10    'head_trap_oo';
+    3 2 9 10    'head_trap_oo';
+    3 3 9 11    'head_trap_oo';
+    2 4 5 10    'head_trap_oi'; % orthogonal, inward
+    3 4 10 10   'head_trap_oi';
+    3 5 10 11   'head_trap_oi';
+    4 4 5 12    'head_trap_oi';
+    5 4 10 12   'head_trap_oi';
+    5 5 10 13   'head_trap_oi';
+    4 6 11 12   'head_trap_oo';
+    5 6 12 12   'head_trap_oo';
+    5 11 12 13  'head_trap_oo';
+    6 6 11 14   'head_trap_oo';
+    7 6 12 14   'head_trap_oo';
+    7 11 12 15  'head_trap_oo';
+    6 7 13 14   'head_trap_oi';
+    7 7 14 14   'head_trap_oi';
+    7 13 14 15  'head_trap_oi';
+    8 7 14 16   'head_trap_oo';
+    8 8 16 16   'head_trap_oi';
+    
+    % vertical
+    2 2 6 4      'head_trap_piss'; % parallel inward, src on short side
+    2 4 6 6      'head_trap_a';    % antisymmetric
+    4 4 7 6      'head_trap_poss';
+    1 2 4 5      'head_trap_a';
+    3 2 6 5      'head_trap_piss';
+    3 4 6 7      'head_trap_a';
+    5 4 7 7      'head_trap_poss';
+    5 6 7 8      'head_trap_posl'; % parallel outward, src on long side
+    7 6 8 8      'head_trap_a';
+    1 1 5 3      'head_trap_poss';
+    1 3 5 5      'head_trap_a';
+    3 3 11 5     'head_trap_piss';
+    3 5 11 7     'head_trap_a';
+    5 5 13 7     'head_trap_poss';
+    5 11 13 8    'head_trap_posl';
+    7 11 15 8    'head_trap_a';
+    9 1 5 10     'head_trap_poss';
+    9 3 5 12     'head_trap_a';
+    10 3 11 12   'head_trap_piss';
+    10 5 11 14   'head_trap_a';
+    12 5 13 14   'head_trap_poss';
+    12 11 13 16  'head_trap_posl';
+    14 11 15 16  'head_trap_a';
+    9 9 10 12    'head_trap_a';
+    10 9 12 12   'head_trap_piss';
+    10 10 12 14  'head_trap_a';
+    12 10 14 14  'head_trap_piss';
+    12 12 14 16  'head_trap_pisl';
+    14 12 16 16  'head_trap_a';
+    11 9 12 13   'head_trap_piss';
+    11 10 12 15  'head_trap_a';
+    13 10 14 15  'head_trap_poss';
+    
+    % RECTENGULARS
+    24 24 23 22 'neck_rect_-135,45'; % outward up down
+    23 21 22 24 'neck_rect_135,-45'; % inward up down
+    20 20 22 21 'neck_rect_45,-135';
+    19 18 17 18 'neck_rect_-135,45';
+    18 20 19 17 'neck_rect_-45,135';
+    21 23 18 20 'neck_lin_i,-45,135'; % cable inward, src inside
+    22 22 20 19 'neck_lin_o,-45,135'; % cable inward, src outside
+    
+    28 26 25 26 'neck_rect_-135,45';
+    27 26 25 25 'neck_sqr_-135,45';
+    32 28 27 30 'neck_rect_-135,45';
+    31 28 27 29 'neck_sqr_-135,45';
+    
+    };
+
+Optodes = cell2mat(patches(:,1:4));
+Types = strcat('Sheep3A_17x7.',patches(:,5));
+
+end
+
+
+
+
+
+
+
+
+
+
+
+% 
+% clear 
+% grd = [0,0,0,201,0,0,0;0,0,101,0,109,0,0;0,202,0,203,0,209,0;102,0,103,0,110,0,111;0,204,0,205,0,210,0;104,0,105,0,112,0,113;0,206,0,211,0,212,0;106,0,107,0,114,0,115;0,207,0,213,0,214,0;0,0,108,0,116,0,0;0,208,0,215,0,216,0];
+% grdsz = size(grd);
+% isSrc = (grd>100 & grd <200);
+% isDet = (grd>200);
+% idxSrc = find(isSrc);
+% idxDet = find(isDet);
+% 
+% for s = 1:numel(idxSrc)
+%     sn1 = grd(idxSrc(s))-100;
+%     [r,c] = ind2sub(grdsz,idxSrc(s));
+%     
+%     if c+3 < grdsz(2) && isSrc(r,c+4)
+%         sn2 = grd(r,c+4);
+%         
---
+++ +StO2patchTypes/Sheep3A_17x7.m
@@ -0,0 +1,74 @@
+function rho = Sheep3A_17x7(patchNam)
+% patches for 3rd sheep, 8.4.2022
+
+import StO2patchTypes.FNC.trapz_rho
+import StO2patchTypes.FNC.arbitr_rho
+
+ptchIds = strsplit(patchNam,'_');
+
+[ptype,config] = ptchIds{2:3};
+
+
+d0 = sqrt(2)*18;
+
+switch ptype
+    case 'trap'
+        % trapezoid -------------------------------------------------------
+        if strcmpi(config,'a') % antisymmetric . . . . . . . . . . . . . . 
+            rho = trapz_rho(d0,2*d0,18,'anti');
+            
+        elseif startsWith(config,'o') % sym, othogonal . . . . . . . . . . 
+            rho = trapz_rho(d0,2*d0,18,'sym','ortho',config(2));
+        
+        elseif startsWith(config,'p') % sym, paralllel . . . . . . . . . . 
+            if endsWith(config,'ss') % src on short side
+                rho = trapz_rho(d0, 2*d0, 18, 'sym', 'para', config(2));
+            elseif endsWith(config,'sl') % src on long side
+                rho = trapz_rho(2*d0, d0, 18, 'sym', 'para', config(2));
+            else
+                error('Unexpected trapezoid configuration code in patch ''%s''.', patchNam);
+            end
+            
+        else
+            error('Unexpected source orientation code in patch ''%s''.', patchNam);
+        end
+            
+        
+    case {'sqr'}
+        % square, diagonal src rotation! ----------------------------------
+        angl = str2double(strsplit(config,','));
+        assert(all(isfinite(angl)),'Failed to get source rotation from patch ''%s''.',patchNam);
+        rho = arbitr_rho([0 18],[18 18],[18 0],angl(1),angl(2));
+%         switch config
+%             case 'oup' % outward up down
+%                 rho = arbitr_rho([0 18],[18 18],[18 0],-135,45);
+%                 
+%             otherwise
+%                 error('Square configuration code in patch ''%s'' not implemented.', patchNam);
+%         end
+        
+        
+    case {'rect'}
+        % rectangular, diagonal src rotation! -----------------------------
+        angl = str2double(strsplit(config,','));
+        assert(all(isfinite(angl)),'Failed to get source rotation from patch ''%s''.',patchNam);
+        rho = arbitr_rho([0 36],[18 36],[18 0],angl(1),angl(2));
+        
+        
+    case 'lin'
+        % linear, diagonal src rotation! ----------------------------------
+        cfgCell = strsplit(config,',');
+        angl = str2double(cfgCell(2:3));
+        assert(all(isfinite(angl)),'Failed to get source rotation from patch ''%s''.',patchNam);
+        switch cfgCell{1}
+            case 'o', rho = arbitr_rho([0 -18], [0 -36], [0 -54], angl(1), angl(2));
+            case 'i', rho = arbitr_rho([0  18], [0 -54], [0 -36], angl(1), angl(2));
+            otherwise, error('Unexpected source position code in patch ''%s''.', patchNam);
+        end
+        
+        
+    otherwise, error('Unrecognized patch name');
+end
+
+
+end
