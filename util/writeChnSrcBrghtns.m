15b096a6e3227803337508c2dbba324ba05e7a63
nrad-NIRS <konrad.schumacher@uniklinik-freiburg.de>
2022-07-28 12:20:10 +0200

    initial

---
+++ util/writeChnSrcBrghtns.m
@@ -0,0 +1,26 @@
+function writeChnSrcBrghtns(cfgFile,outFile)
+%
+
+assert(exist(cfgFile,'file'),'File not found: ''%s''',cfgFile);
+cfg = loadjson(cfgFile);
+
+chnMask = cfg.channel_mask;
+drv_plan = cfg.drv_plan;
+switch cfg.modulation
+    case 'rect'
+        brgth_plan = cfg.amp_plan;
+    case 'sine'
+        brgth_plan = cfg.drv_amplitudes;
+    otherwise, error('Unexpected modulation ''%s'' in ''%s''',cfg.modulation,cfgFile);
+end
+
+[chnList, chnBrghtns] = getChnSrcBrghtns(chnMask,brgth_plan,drv_plan);
+
+D = [chnList num2cell(chnBrghtns)].';
+
+[fid,errmsg] = fopen(outFile,'w');
+assert(fid>2,'Failed to open file ''%s'' for writing because:\n%s',outFile,errmsg);
+fprintf(fid,'%s,%.9f,%.6f\n',D{:});
+fclose(fid);
+
+end
