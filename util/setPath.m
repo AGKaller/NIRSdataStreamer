6b300417c62ee41baf19af9c8e7db3424995dac4
schumach <schumach@NRADN010.ad.uniklinik-freiburg.de>
2024-12-02 09:43:12 +0100

    added NRADN010; a bit clean up

--- util/setPath.m
+++ util/setPath.m
@@ -5,43 +5,45 @@ if nargin<1
     project = '';
 end
 
-afsNSP2path = '\\AFS\fbi.ukl.uni-freiburg.de\projects\CascadeNIRS\test\202101_NIRSport2_test\';
+% afsNSP2path = '\\AFS\fbi.ukl.uni-freiburg.de\projects\CascadeNIRS\test\202101_NIRSport2_test\';
 
 p.outPath = fullfile('W:\Data\NIRSport2Data\NIRS_PERFUSION\',project);
+rootPth = fileparts(fileparts(fileparts(mfilename('fullpath'))));
+appPth = fileparts(fileparts(mfilename('fullpath')));
+p.optodeLayouts = fullfile(appPth, 'optodeLayouts');
 
 switch getenv('COMPUTERNAME')
     case 'NRAD-NIRS' % ='NIRS'
-        dsPth = fileparts(fileparts(mfilename('fullpath'))); % 'C:\Users\nradu\Documents\MATLAB';
-        rootPth = fileparts(fileparts(fileparts(mfilename('fullpath')))); % 'C:\Users\nradu\Documents\MATLAB';
+         % 'C:\Users\nradu\Documents\MATLAB';
+         % 'C:\Users\nradu\Documents\MATLAB';
         addpath(genpath(fullfile(rootPth,'liblsl-Matlab')));
         addpath(fullfile(rootPth,'jsonlab-2.0'));
         addpath(fullfile(rootPth,'TriggerCtrlGUI','src'));
-        p.nspConfigPth = fullfile('C:\Users\nradu\Documents','NIRx','Configurations');% fullfile(fileparts(userpath),'NIRx','Configurations');
-        p.optodeLayouts = fullfile(dsPth, 'optodeLayouts');
+        % p.nspConfigPth = fullfile('C:\Users\nradu\Documents','NIRx','Configurations');% fullfile(fileparts(userpath),'NIRx','Configurations');
+        
         
     case {'NRAD-NIRS-W11','NRADC033','FRONIN001'} % = 'NIRS'
-        dsPth = fileparts(fileparts(mfilename('fullpath'))); % app path
-        rootPth = fileparts(fileparts(fileparts(mfilename('fullpath')))); % matlab path 'C:\Users\<user>\Documents\MATLAB';
         addpath(genpath(fullfile(rootPth,'liblsl-Matlab')));
         addpath(fullfile(rootPth,'jsonlab-2.0'));
         % addpath(fullfile(rootPth,'TriggerCtrlGUI','src'));
         % deprecated?!:
         % p.nspConfigPth = fullfile('C:\Users\NIRS_\Documents','NIRx','Configurations');% fullfile(fileparts(userpath),'NIRx','Configurations');
-        p.optodeLayouts = fullfile(dsPth, 'optodeLayouts');
         
     case 'CHIMERA'
-        dsPth = fileparts(fileparts(mfilename('fullpath'))); % 'C:\Users\nradu\Documents\MATLAB';
-        rootPth = fileparts(fileparts(fileparts(mfilename('fullpath')))); % 'C:\Users\nradu\Documents\MATLAB';
         addpath(genpath(fullfile(rootPth,'liblsl-Matlab')));
         addpath(fullfile(rootPth,'jsonlab-2.0'));
 %         addpath(fullfile(rootPth,'TriggerCtrlGUI','src'));
-        p.nspConfigPth = fullfile(afsNSP2path,'Configurations');
-        p.optodeLayouts = fullfile(dsPth, 'optodeLayouts');
+        % p.nspConfigPth = fullfile(afsNSP2path,'Configurations');
         if strcmpi(getenv('USERNAME'),'konrad')
             p.outPath = fullfile('W:\home\nirs\Data\NIRSport2Data\NIRS_PERFUSION\',project);
         end
 
-        
+    case {'NRADN010'}
+        rootPth = userpath;
+        tbPath = fullfile(rootPth,'toolboxes');
+        addpath(genpath(fullfile(tbPath,'liblsl-Matlab')));
+        addpath(fullfile(tbPath,'jsonlab-2.0'));
+        p.outPath = fullfile('Z:\home\nirs\Data\NIRSport2Data\NIRS_PERFUSION\',project);
     otherwise, error('Unknown COMPUTERNAME, can''t set paths.');
 end
 
