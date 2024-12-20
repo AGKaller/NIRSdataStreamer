335fa0a2d8355ad6b44421d5c3ac811079b01a34
NIRS_ <NIRS_@10.195.56.37>
2024-12-20 17:48:40 +0100

    added Samba network drive for NRAD NIRS W11 Laptop

--- util/setPath.m
+++ util/setPath.m
@@ -30,7 +30,7 @@ switch pcName
         % deprecated?!:
         % p.nspConfigPth = fullfile('C:\Users\NIRS_\Documents','NIRx','Configurations');% fullfile(fileparts(userpath),'NIRx','Configurations');
         
-        if ~strcmp(pcName,'NRAD-NIRS-W11'), p.outPath = fullfile('P:\',project); end
+        p.outPath = fullfile('P:\',project);
         
     case 'CHIMERA'
         addpath(genpath(fullfile(rootPth,'liblsl-Matlab')));
